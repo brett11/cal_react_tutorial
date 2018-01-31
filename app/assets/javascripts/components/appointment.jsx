be8b61eb1b749bcf8031f01fa136360c923ffc3d
Brett F <brettfox11@gmail.com>
2018-01-31 09:17:55 -0600

    Add some css styling

--- app/assets/javascripts/components/appointment.jsx
+++ app/assets/javascripts/components/appointment.jsx
@@ -5,7 +5,7 @@ class Appointment extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="appointment">
                 <h3>{this.props.appointment.title}</h3>
                 <p>{formatDate(this.props.appointment.appt_time)}</p>
             </div>
